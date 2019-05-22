5b1ce378ed5e696ac6e9848fbc25b11a632ab041
forbid403 <forbid403@naver.com>
2019-05-22 21:59:42 +0900

    0522FT : cut date

--- asahi.py
+++ asahi.py
@@ -1,12 +1,49 @@
 import requests as rq
+import pymongo
+from pymongo import MongoClient
 from bs4 import BeautifulSoup
+import module
 
+##mongo db connect
+port = 27017;
+conn = pymongo.MongoClient('localhost', port)
+momentDB = conn.get_database("moment")
+print("moment db loaded : ", momentDB)
+
+japanCollection = momentDB.get_collection("japan")
+print("japan collection loaded : ", japanCollection)
+
+##crawling
 asahi_url = 'https://www.asahi.com/news/?iref=comtop_gnavi'
 
 res = rq.get(asahi_url)
 soup = BeautifulSoup(res.content, 'html.parser')
 
-for category_ul in soup.find_all("ul", class_="List"):
-    for link in soup.find_all("a", class_="SW"):
-        print(link.text)
+for tag in soup.select("a.SW"):
+    for list in tag.select("span.Time"):
+        print(list.text)
+
+for temp in soup.select("a.SW > span"):
+    temp.decompose()
+
+news = soup.select("a.SW > span")
+for new in news:
+    print(new.text.strip())
+
+#날짜랑 기사 제목 따로 DB 저장하기
+
+client = MongoClient('localhost', 27017)
+db = client["moment"]
+collection = db["Japan"]
+
+post_jpn = {
+    "writer" : "writer",
+    "subject" : "subject",
+    "time" : "",
+    "field" : []
+}
+
+returned_date = module.cutDate()
+print(returned_date)
+#collection.insert(post_jpn)
 
---
+++ module.py
@@ -0,0 +1,20 @@
+import datetime
+
+def cutDate():
+    date_string = "(5/21 21:04)"
+    temp00 = date_string.replace("(", "")
+    temp0 = temp00.replace(")", "")
+    temp1 = temp0.split('/')
+    month = int(temp1[0])
+
+    temp2 = temp1[1]
+    temp3 = temp2.split(" ")
+    day = int(temp3[0])
+
+    temp4 = temp3[1]
+    temp5 = temp4.split(":")
+    hour = int(temp5[0])
+    minute = int(temp5[1])
+
+    return datetime.datetime(2019, month, day, hour, minute)
+
